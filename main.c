3d3b8a09967ac855e0c856886c23c2f0583d8df6
milagros1010 <40776605+milagros1010@users.noreply.github.com>
2018-09-01 12:31:26 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,29 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    int nro1,nro2,nro3,dato;
-    printf("ingrese");
-    scanf("%d", & nro1);
-    printf("ingrese");
-    scanf("%d", & nro2);
-    printf("ingrese");
-    scanf("%d", & nro3);
+    int flag, opcion;
 
-    if(nro1>nro2&& nro3>nro1)
+    do
     {
-       dato = nro1;
-    }
-    else if(nro2>nro1 && nro2<nro3)
-    {
-        dato = nro2;
-    }
-    else
-    {
-        dato = nro3;
-    }
-    printf("el valor del medio es: %d",dato);
+        printf("1.ingresar 1er operando(A=x)\n 2. Ingresar 2do operando\n: ");
+        printf("\n Ingrese una opcion del menu: ");
+        scanf("%d",&opcion);
+        __fpurge(stdin);
+
+
+        switch(opcion)
+        {
+            case 1:
+                ingresarNumeros(&numero1);
+            break;
+            case 2:
+                ingresarNumeros(&numero2);
+            case 3:
+                suma();
+                resta();
+                division();
+                printf("calculando..")
+            break;
+            case 4:
+
+            case 5:
+                SALIR;
+            break;
+            flag = 0;
+            default:
+             printf("ingrese una opcion valida!\n");
+
+        }
+        __fpurge(stdin);// limpia el buffer
+        printf("\n Ingrese ENTER para continuar...");
+        getchar();
+        system("clear");
+
+    } while(flag!=0);
+
     return 0;
+}
 
+int ingresarNumero (float* numero)
+{
+    printf("Ingrese un numero: ");
+    scanf("%d"&)
 }
